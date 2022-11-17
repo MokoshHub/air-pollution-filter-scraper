11fe9e5a0f707a186348debb6215dac4f81b723f
saras <lost.func@gmail.com>
2022-11-17 21:45:37 +0100

    reformatted w black

--- data_scraper.py
+++ data_scraper.py
@@ -7,6 +7,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from gcloud import storage
 
+
 def download_data_by_date(date, data_path="pms_data"):
     # DATE = '2022-11-10'
     # DATA_PATH = 'pms_data'
@@ -27,15 +28,20 @@ def download_data_by_date(date, data_path="pms_data"):
     if not os.path.exists(os.path.join(data_path, date)):
         os.makedirs(os.path.join(data_path, date))
 
-    driver.get(('http://archive.sensor.community/%s/' % date));
+    driver.get(("http://archive.sensor.community/%s/" % date))
     files_list = driver.find_elements(By.XPATH, "/html/body/pre[2]")[0]
     a_tags = files_list.find_elements(By.TAG_NAME, "a")[1:]
 
     for a in tqdm(a_tags):
-        if ('indoor' not in a.get_attribute("href") and 'sds011' in a.get_attribute("href")):
-            wget.download(a.get_attribute("href"), os.path.join(data_path, date), bar=None)
+        if "indoor" not in a.get_attribute("href") and "sds011" in a.get_attribute(
+            "href"
+        ):
+            wget.download(
+                a.get_attribute("href"), os.path.join(data_path, date), bar=None
+            )
 
     driver.quit()
+
 
 def clean_data(date, data_path="pms_data"):
     # DATA_PATH = 'pms_data'
@@ -45,10 +51,12 @@ def clean_data(date, data_path="pms_data"):
         for file in tqdm(files):
             if file.endswith(".csv"):
                 with open(os.path.join(root, file)) as csv_file:
-                    single_sensor_file = csv.reader(csv_file, delimiter=';')
-                    next(single_sensor_file) # skip first line
+                    single_sensor_file = csv.reader(csv_file, delimiter=";")
+                    next(single_sensor_file)  # skip first line
                     for row in tqdm(single_sensor_file, leave=False):
-                        with jsonlines.open(os.path.join(data_path, 'mika' + '.jsonl'), mode='a') as writer:
+                        with jsonlines.open(
+                            os.path.join(data_path, "mika" + ".jsonl"), mode="a"
+                        ) as writer:
                             sensor_id = row[0]
                             timestamp = row[5]
                             lat = row[3]
@@ -56,38 +64,41 @@ def clean_data(date, data_path="pms_data"):
                             P1 = row[6]
                             P2 = row[9]
 
-                            writer.write({
-                                'sensor_id': sensor_id,
-                                'timestamp': timestamp,
-                                'lat': lat,
-                                'lon': lon,
-                                'P1': P1,
-                                'P2': P2
-                            })
+                            writer.write(
+                                {
+                                    "sensor_id": sensor_id,
+                                    "timestamp": timestamp,
+                                    "lat": lat,
+                                    "lon": lon,
+                                    "P1": P1,
+                                    "P2": P2,
+                                }
+                            )
             # break
+
 
 def get_sensors_last_five_mins(data_path):
     # past 5 minutes URL
     # https://data.sensor.community/airrohr/v1/filter/type=SDS011/
-    past_5_mins_url = 'https://data.sensor.community/airrohr/v1/filter/type=SDS011'
+    past_5_mins_url = "https://data.sensor.community/airrohr/v1/filter/type=SDS011"
     response = requests.get(past_5_mins_url)
     data = response.json()
-    
+
     # change to append mode when script is running autonomously
-    with jsonlines.open(os.path.join(data_path, 'mika' + '.jsonl'), mode='a') as writer:
+    with jsonlines.open(os.path.join(data_path, "mika" + ".jsonl"), mode="a") as writer:
         for entry in data:
-            sensor_id = entry['sensor']['id']
+            sensor_id = entry["sensor"]["id"]
             # timestamp is 1 hour behind
             # timestamp = str(fix_timestamp(entry['timestamp']))
-            timestamp = entry['timestamp']
-            lat = entry['location']['latitude']
-            lon = entry['location']['longitude']
-            
+            timestamp = entry["timestamp"]
+            lat = entry["location"]["latitude"]
+            lon = entry["location"]["longitude"]
+
             try:
-                if entry['sensordatavalues'][0]['value_type'] == 'P1':
-                    p1 = entry['sensordatavalues'][0]['value']
-                elif entry['sensordatavalues'][1]['value_type'] == 'P1':
-                    p1 = entry['sensordatavalues'][1]['value']
+                if entry["sensordatavalues"][0]["value_type"] == "P1":
+                    p1 = entry["sensordatavalues"][0]["value"]
+                elif entry["sensordatavalues"][1]["value_type"] == "P1":
+                    p1 = entry["sensordatavalues"][1]["value"]
 
                 # remove outliers from data
                 if float(p1) >= 1999.90:
@@ -96,10 +107,10 @@ def get_sensors_last_five_mins(data_path):
                 p1 = None
 
             try:
-                if entry['sensordatavalues'][0]['value_type'] == 'P2':
-                    p2 = entry['sensordatavalues'][0]['value']
-                elif entry['sensordatavalues'][1]['value_type'] == 'P2':
-                    p2 = entry['sensordatavalues'][1]['value']
+                if entry["sensordatavalues"][0]["value_type"] == "P2":
+                    p2 = entry["sensordatavalues"][0]["value"]
+                elif entry["sensordatavalues"][1]["value_type"] == "P2":
+                    p2 = entry["sensordatavalues"][1]["value"]
 
                 # remove outliers from data
                 if float(p2) >= 999.90:
@@ -108,52 +119,60 @@ def get_sensors_last_five_mins(data_path):
                 p2 = None
 
             if p1 != None and p2 != None:
-                writer.write({
-                    'sensor_id': sensor_id,
-                    'timestamp': timestamp,
-                    'lat': lat,
-                    'lon': lon,
-                    'P1': p1,
-                    'P2': p2
-                })
+                writer.write(
+                    {
+                        "sensor_id": sensor_id,
+                        "timestamp": timestamp,
+                        "lat": lat,
+                        "lon": lon,
+                        "P1": p1,
+                        "P2": p2,
+                    }
+                )
+
 
 def clean_old_sensors(DATA_PATH):
     # delete sensor data from more than 48h ago
-    with jsonlines.open(os.path.join(DATA_PATH, 'mika' + '.jsonl'), mode='r') as reader:
+    with jsonlines.open(os.path.join(DATA_PATH, "mika" + ".jsonl"), mode="r") as reader:
         fresh_lines = []
         for entry in reader:
             fresh_lines.append(entry)
 
-    with jsonlines.open(os.path.join(DATA_PATH, 'mika' + '.jsonl'), mode='w') as updater:
+    with jsonlines.open(
+        os.path.join(DATA_PATH, "mika" + ".jsonl"), mode="w"
+    ) as updater:
         for entry in fresh_lines:
-            timestamp = datetime.strptime(entry['timestamp'], '%Y-%m-%d %H:%M:%S')
+            timestamp = datetime.strptime(entry["timestamp"], "%Y-%m-%d %H:%M:%S")
             current_time = datetime.utcnow()
             if not current_time - timestamp > timedelta(hours=48):
                 updater.write(entry)
+
 
 def fix_timestamp(timestamp):
     # check if timestamp 1 hour behind
     # if so, add 1 hour
     # else, return timestamp
-    timestamp = datetime.strptime(timestamp, '%Y-%m-%d %H:%M:%S')
+    timestamp = datetime.strptime(timestamp, "%Y-%m-%d %H:%M:%S")
     current_time = datetime.now()
 
     if timestamp.hour == current_time.hour - 1:
         timestamp = timestamp + timedelta(hours=1)
-        return timestamp.strftime('%Y-%m-%d %H:%M:%S')
+        return timestamp.strftime("%Y-%m-%d %H:%M:%S")
     else:
-        return timestamp.strftime('%Y-%m-%d %H:%M:%S')
+        return timestamp.strftime("%Y-%m-%d %H:%M:%S")
+
 
 def push_data_to_bucket(DATA_PATH):
     # push data to bucket
-    client = storage.Client.from_service_account_json('creds.json')
-    bucket = client.get_bucket('mika_air_quality')
-    blob = bucket.blob('mika.jsonl')
-    blob.cache_control = 'no-cache'
-    blob.upload_from_filename(os.path.join(DATA_PATH, 'mika' + '.jsonl'))
+    client = storage.Client.from_service_account_json("creds.json")
+    bucket = client.get_bucket("mika_air_quality")
+    blob = bucket.blob("mika.jsonl")
+    blob.cache_control = "no-cache"
+    blob.upload_from_filename(os.path.join(DATA_PATH, "mika" + ".jsonl"))
+
 
 def main():
-    DATA_PATH = './pms_data'
+    DATA_PATH = "./pms_data"
     # DATE = '2022-11-10'
 
     if not os.path.exists(DATA_PATH):
@@ -164,6 +183,7 @@ def main():
     get_sensors_last_five_mins(DATA_PATH)
     clean_old_sensors(DATA_PATH)
     push_data_to_bucket(DATA_PATH)
+
 
 if __name__ == "__main__":
     main()
